16f4849bdcbb4a96830224eb9d33b4c8304160da
nshugay <nshugay1@gmail.com>
2024-08-25 15:51:40 +0300

    feat: pass test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+  let newStr;
+
+  do {
+      newStr = str.length;
+
+      for (let [x, y] of bracketsConfig) {
+          str = str.replace(x + y, '')
+          // console.log(str)
+      };
+
+  } while (str.length < newStr);
+
+  return str.length === 0;
+};
